7b14bae5f4adb613afc21504be4934a03bf05f58
Cosme Stakemann <cosme.stakemann@alegra.com>
2024-08-11 21:14:11 -0400

    test

--- src/events/dto/update-event.dto.ts
+++ src/events/dto/update-event.dto.ts
@@ -1,15 +1,15 @@
 import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateEventDto } from './create-event.dto';
-import { IsOptional, IsString } from 'class-validator';
+import { IsArray, IsOptional, IsString, ValidateNested } from 'class-validator';
 import { EventImage } from './imageName.dto';
 import { Type } from 'class-transformer';
 
 export class UpdateEventDto extends PartialType(CreateEventDto) {
 
-    // @Type(()=> EventImage)
-    @IsString()
-    @ApiProperty()
-    currentImages: string
+    @IsArray()
+    @ValidateNested({ each: true })
+    @Type(() => EventImage)
+    currentImages: EventImage[];
     
     @IsString()
     @IsOptional()
